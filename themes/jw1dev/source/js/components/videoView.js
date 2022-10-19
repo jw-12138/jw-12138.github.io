01f8e67f975b8422acbcca69d665715f05f354e2
Jacky Wong <29943110+jw-12138@users.noreply.github.com>
2022-10-19 22:07:09 +0800

    fix: video component is not rendered correctly

--- themes/jw1dev/source/js/components/videoView.js
+++ themes/jw1dev/source/js/components/videoView.js
@@ -11,9 +11,8 @@ export default {
     }
   },
   template: `
-    <div>
+    <template>
     <video playsinline autoplay muted controls loop :src="src" preload="metadata" style="max-width: 100%; width: auto;display: block;"></video>
-    <noscript>请开启JS功能以查看视频</noscript>
-    </div>
+    </template>
   `
 }
