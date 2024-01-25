ea89ae20a6d11ebac101dfe8e3a5d537fa6d483e
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-01-25 17:55:13 +0800

    feat: add toc for reading view

---
+++ src/components/toc.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="lg:block hidden mt-8 relative left-[-.5rem]">
+    <div class="px-2 py-1 rounded-xl hover:bg-black/10 dark:hover:bg-white/10 transition-all w-8 h-8 flex justify-center items-center mb-1 cursor-pointer opacity-70 hover:opacity-100"
+         @click="goBackToTop"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-navigation-top" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.54 19.977a.34 .34 0 0 0 .357 -.07a.33 .33 0 0 0 .084 -.35l-4.981 -10.557l-4.982 10.557a.33 .33 0 0 0 .084 .35a.34 .34 0 0 0 .357 .07l4.541 -1.477l4.54 1.477z" /><path d="M12 3v2" /></svg>
+    </div>
+    <div
+      @click="scrollToSlug(heading.slug)"
+      :title="heading.text"
+      class="overflow-hidden text-ellipsis whitespace-nowrap text-xs max-w-full cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 pt-[.35rem] pb-[.31rem] px-2 transition-all"
+      v-for="(heading, index) in data"
+      :class="{
+        'rounded-t-xl hover:rounded-b-0': (!data[index - 1]) || (data[index - 1] && !slugPositionMap[data[index - 1].slug]?.isInView),
+        'rounded-b-xl hover:rounded-t-0': (!data[index + 1]) || (data[index + 1] && !slugPositionMap[data[index + 1].slug]?.isInView),
+        'text-black dark:text-white opacity-1 bg-black/10 dark:bg-white/10': slugPositionMap[heading.slug]?.isInView,
+        'opacity-70 hover:rounded-xl': !slugPositionMap[heading.slug]?.isInView
+      }"
+      :style="{
+      paddingLeft: (heading.depth - maxDepth + 1) * .5 + 'rem'
+    }">
+      {{ heading.text }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {onMounted, ref, reactive} from 'vue'
+
+const slugPositionMap = reactive({})
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => []
+  }
+})
+
+let maxDepth = ref(6)
+
+function findMaxDepth() {
+  if (props.data.length === 0) {
+    return false
+  }
+
+  props.data.map(el => {
+    if (el.depth < maxDepth.value) {
+      maxDepth.value = el.depth
+    }
+  })
+}
+
+function goBackToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+function scrollToSlug(slug) {
+  const dom = document.getElementById(slug)
+  if (dom) {
+    window.scroll({
+      top: dom.offsetTop + 10,
+      behavior: 'smooth'
+    })
+  }
+}
+
+function getPositionForEachSlug() {
+  let windowScrollTop = document.documentElement.scrollTop || document.body.scrollTop
+  let windowHeight = document.documentElement.clientHeight || document.body.clientHeight
+  props.data.map((el, index) => {
+    const slug = el.slug
+    try {
+      const dom = document.getElementById(slug)
+      const top = dom.offsetTop
+      let nextHeadingTop
+      if (props.data[index + 1]) {
+        nextHeadingTop = document.getElementById(props.data[index + 1].slug).offsetTop
+      } else {
+        nextHeadingTop = document.querySelector('.page-content').offsetHeight + 156
+      }
+
+      let range = [top - windowScrollTop, nextHeadingTop - windowScrollTop]
+      let isInView = range[0] < windowHeight && range[1] > 0
+      slugPositionMap[slug] = {
+        isInView
+      }
+    } catch (e) {
+
+    }
+  })
+}
+
+onMounted(async () => {
+  await new Promise(resolve => setTimeout(resolve, 100))
+  findMaxDepth()
+  getPositionForEachSlug()
+
+  window.addEventListener('scroll', () => {
+    getPositionForEachSlug()
+  })
+
+  window.addEventListener('resize', () => {
+    getPositionForEachSlug()
+  })
+})
+</script>
