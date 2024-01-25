645a1f3e0e1cf732588a81ac143f0dd222b98153
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-01-25 18:27:00 +0800

    feat: add toc for reading view

--- src/components/toc.vue
+++ src/components/toc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lg:block hidden mt-8 relative left-[-.5rem]">
-    <div class="px-2 py-1 rounded-xl hover:bg-black/10 dark:hover:bg-white/10 transition-all w-8 h-8 flex justify-center items-center mb-1 cursor-pointer opacity-70 hover:opacity-100"
+    <div class="px-2 py-1 rounded-xl hover:bg-black/15 dark:hover:bg-white/15 transition-all w-8 h-8 flex justify-center items-center mb-1 cursor-pointer opacity-70 hover:opacity-100"
          @click="goBackToTop"
     >
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-navigation-top" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.54 19.977a.34 .34 0 0 0 .357 -.07a.33 .33 0 0 0 .084 -.35l-4.981 -10.557l-4.982 10.557a.33 .33 0 0 0 .084 .35a.34 .34 0 0 0 .357 .07l4.541 -1.477l4.54 1.477z" /><path d="M12 3v2" /></svg>
@@ -8,18 +8,20 @@
     <div
       @click="scrollToSlug(heading.slug)"
       :title="heading.text"
-      class="overflow-hidden text-ellipsis whitespace-nowrap text-xs max-w-full cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 pt-[.35rem] pb-[.31rem] px-2 transition-all"
+      class="text-xs max-w-full cursor-pointer hover:bg-black/15 dark:hover:bg-white/15 px-2 transition-all duration-500"
       v-for="(heading, index) in data"
       :class="{
-        'rounded-t-xl hover:rounded-b-0': (!data[index - 1]) || (data[index - 1] && !slugPositionMap[data[index - 1].slug]?.isInView),
-        'rounded-b-xl hover:rounded-t-0': (!data[index + 1]) || (data[index + 1] && !slugPositionMap[data[index + 1].slug]?.isInView),
+        'rounded-t-xl hover:rounded-b-0 pt-[.3rem]': (!data[index - 1]) || (data[index - 1] && !slugPositionMap[data[index - 1].slug]?.isInView),
+        'rounded-b-xl hover:rounded-t-0 pb-[.25rem]': (!data[index + 1]) || (data[index + 1] && !slugPositionMap[data[index + 1].slug]?.isInView),
         'text-black dark:text-white opacity-1 bg-black/10 dark:bg-white/10': slugPositionMap[heading.slug]?.isInView,
-        'opacity-70 hover:rounded-xl': !slugPositionMap[heading.slug]?.isInView
+        'opacity-70 hover:rounded-xl pt-[.25rem] pb-[.21rem]': !slugPositionMap[heading.slug]?.isInView
       }"
       :style="{
       paddingLeft: (heading.depth - maxDepth + 1) * .5 + 'rem'
     }">
-      {{ heading.text }}
+      <div class="overflow-hidden text-ellipsis whitespace-nowrap py-[.1rem]">
+        {{ heading.text }}
+      </div>
     </div>
   </div>
 </template>
@@ -98,12 +100,8 @@ onMounted(async () => {
   findMaxDepth()
   getPositionForEachSlug()
 
-  window.addEventListener('scroll', () => {
+  setInterval(() => {
     getPositionForEachSlug()
-  })
-
-  window.addEventListener('resize', () => {
-    getPositionForEachSlug()
-  })
+  }, 1000 / 24)
 })
 </script>
