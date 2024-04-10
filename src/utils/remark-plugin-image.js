758ac42b406c92d6781adbb535e148c46c207fe4
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-04-10 19:27:50 +0800

    feat: add video and params support in remark image plugin

--- src/utils/remark-plugin-image.js
+++ src/utils/remark-plugin-image.js
@@ -1,17 +1,50 @@
 import {visit} from 'unist-util-visit'
 
-export default function remarkPlugin() {
+export default function remarkPluginImage() {
   return (tree) => {
     visit(tree, 'image', (node) => {
-      if (!node.data) {
-        node.data = {}
-      }
+      if (node.alt === 'video' || node.url.endsWith('.mp4')) {
+        node.type = 'html'
+        node.value = `<video src="${node.url}" controls playsinline loop muted></video>`
+        node.children = undefined
+      } else {
+        if (!node.data) {
+          node.data = {}
+        }
 
-      if (!node.data.hProperties) {
-        node.data.hProperties = {}
-      }
+        if (!node.data.hProperties) {
+          node.data.hProperties = {}
+        }
 
-      node.data.hProperties.loading = "lazy"
+        node.data.hProperties.loading = 'lazy'
+
+        const paramsWhiteList = [
+          'width',
+          'height'
+        ]
+        if(node.url.split('?')[1]){
+          let [url, params] = node.url.split('?')
+
+          node.url = url
+
+          let paramsObj = {}
+          params.split('&').forEach((param) => {
+            let [key, value] = param.split('=')
+            if (paramsWhiteList.includes(key)) {
+              paramsObj[key] = value
+            }
+          })
+
+          let style = ''
+
+          // loop the keys in the paramsObj and add them to the style string
+          for (let key in paramsObj) {
+            style += `${key}:${paramsObj[key]};`
+          }
+
+          node.data.hProperties.style = style
+        }
+      }
     })
   }
 }
