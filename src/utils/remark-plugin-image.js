2e397b90e83d04d4581978d1a9242fdcd59bce15
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-04-11 16:37:07 +0800

    feat: new image render logic

--- src/utils/remark-plugin-image.js
+++ src/utils/remark-plugin-image.js
@@ -3,47 +3,48 @@ import {visit} from 'unist-util-visit'
 export default function remarkPluginImage() {
   return (tree) => {
     visit(tree, 'image', (node) => {
+      node.type = 'html'
+      node.children = undefined
       if (node.alt === 'video' || node.url.endsWith('.mp4')) {
-        node.type = 'html'
         node.value = `<video src="${node.url}" controls playsinline loop muted></video>`
-        node.children = undefined
       } else {
-        if (!node.data) {
-          node.data = {}
-        }
-
-        if (!node.data.hProperties) {
-          node.data.hProperties = {}
-        }
-
-        node.data.hProperties.loading = 'lazy'
-
-        const paramsWhiteList = [
+        const paramsStyleWhiteList = [
           'width',
           'height'
         ]
+
+        const paramsElementWhiteList = [
+          'figcaption'
+        ]
+
+        let style = ''
+        let nodeUrl = node.url
+        let figure = ''
+
         if(node.url.split('?')[1]){
           let [url, params] = node.url.split('?')
 
-          node.url = url
+          nodeUrl = url
 
           let paramsObj = {}
           params.split('&').forEach((param) => {
             let [key, value] = param.split('=')
-            if (paramsWhiteList.includes(key)) {
+            if (paramsStyleWhiteList.includes(key)) {
               paramsObj[key] = value
             }
-          })
 
-          let style = ''
+            if (paramsElementWhiteList.includes(key)) {
+              figure = `<span class="figure">${node.alt}</span>`
+            }
+          })
 
           // loop the keys in the paramsObj and add them to the style string
           for (let key in paramsObj) {
             style += `${key}:${paramsObj[key]};`
           }
-
-          node.data.hProperties.style = style
         }
+
+        node.value = `<img src="${nodeUrl}" alt="${node.alt}" style="${style}" loading="lazy"/> ${figure}`
       }
     })
   }
