df3bd3e6f43549b2efb909f00898d8ff9bd22c15
Jacky Wong <29943110+jw-12138@users.noreply.github.com>
2024-04-18 23:55:42 +0800

    feat: remove vue, embracing solid-js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import vue from '@astrojs/vue';
 import remarkPluginImage from './src/utils/remark-plugin-image.js';
 import remarkPluginText from './src/utils/remark-plugin-text.js';
 import remarkPluginLink from './src/utils/remark-plugin-link.js';
@@ -9,7 +8,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), vue(), solidJs()],
+  integrations: [tailwind(), mdx(), solidJs()],
   site: 'https://jw1.dev',
   markdown: {
     gfm: false,
