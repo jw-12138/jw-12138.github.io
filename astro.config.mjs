e1579a70534bc29b891d6f895b9c209ab9b5e529
Jacky Wong <29943110+jw-12138@users.noreply.github.com>
2025-04-03 14:12:50 +0800

    refactor: update Astro configuration, remove unused components

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import mdx from '@astrojs/mdx';
 import remarkPluginImage from './src/utils/remark-plugin-image.js';
 import remarkPluginText from './src/utils/remark-plugin-text.js';
 import remarkPluginLink from './src/utils/remark-plugin-link.js';
-import solidJs from "@astrojs/solid-js";
+import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), solidJs()],
+  integrations: [tailwind(), mdx(), solid()],
   site: 'https://jw1.dev',
   markdown: {
     gfm: true,
